1b55a65dd5c5d20f4c2c0bdb4f7d076e28b9c1ad
Reji Mathews 84131876 <reji.mathews@huawei.com>
2021-01-19 15:39:41 -0500

    simple ipv6 route

---
+++ camel-poc/src/main/java/com/camel/poc/camelpoc/TestRoute.java
@@ -0,0 +1,15 @@
+package com.camel.poc.camelpoc;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("jetty:http://[::]:1122/test")
+                .log("received http request")
+                .transform(simple("OK!"))
+                .end();
+    }
+}
---
+++ camel-poc/src/test/java/com/camel/poc/camelpoc/CamelPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.poc.camelpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
